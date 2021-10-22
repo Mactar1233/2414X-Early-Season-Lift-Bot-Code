3a057ab11f2c83cb23f6423d5409d8d357477b37
Mactar_Gueye <Mactarg1233@gmail.com>
2021-10-21 20:26:43 -0700

    dogo copypasta done

--- include/EZ-Template/util.hpp
+++ include/EZ-Template/util.hpp
@@ -5,7 +5,7 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 #pragma once
-#include "api.h"
+#include "include/api.h"
 
 void print_ez_template();
 
--- src/EZ-Template/joystick_control.cpp
+++ src/EZ-Template/joystick_control.cpp
@@ -4,7 +4,7 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-#include "main.h"
+#include "include/main.h"
 
 
 pros::controller_analog_e_t current_l_stick = LEFT_JOYSTICK;
