5ceb2f4beed5c5e0a0b2329611f9006beb36df8e
Mactar_Gueye <Mactarg1233@gmail.com>
2021-11-23 16:43:49 -0800

    redid code with template 2.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,126 +1,46 @@
 #include "main.h"
+
 #include <iostream>
+#include <tuple>
 
 
-/**
- * Disables all tasks.
- *
- * This runs during disabled and initialize to turn off all user created tasks.
- */
-void
-disable_all_tasks() {
-  drive_pid.suspend();
-}
 
+// Chassis constructor
+drive chassis (
+  // Left Chassis Ports (negative port will reverse it!)
+  {-11, -5, -7}
 
-/**
- * Autonomous selector using LLEMU.
- *
- * When this function is called, display the current selected auton to the brain.
- * When is_auton is true, the autonomous mode will run.  Otherwise, it will only
- * print to brain.
- */
-const int num_of_pages = 6; // Number of pages
-int current_page = 0;
+  // Right Chassis Ports (negative port will reverse it!)
+  ,{3, 2, 17}
 
-void
-auto_select(bool is_auton) {
-  for (int i=0; i<7; i++)
-    pros::lcd::clear_line(i);
+  // IMU Port
+  ,18
 
-  pros::lcd::set_text(0, "Page "+std::to_string(current_page+1));
+  // Wheel Diameter (Remember, 4" wheels are actually 4.125!)
+  //    (or tracking wheel diameter)
+  ,3.25
 
-  switch (current_page) {
-    case 0: // Auto 1
-      pros::lcd::set_text(1, "Test Auton");
-      if (is_auton) test_auton();
-      break;
-    case 1: // Auto 2
-      pros::lcd::set_text(1, "Auton 1");
-      if (is_auton) auto_1();
-      break;
-    case 2: // Auto 3
-      pros::lcd::set_text(1, "Auton 2");
-      if (is_auton) auto_2();
-      break;
-    case 3: // Auto 4
-      pros::lcd::set_text(1, "Auton 3");
-      if (is_auton) auto_3();
-      break;
-    case 4: // Auto 5
-      pros::lcd::set_text(1, "Auton 4");
-      if (is_auton) auto_4();
-      break;
-    case 5: // Auto 6
-      pros::lcd::set_text(1, "Auton 5");
-      if (is_auton) auto_5();
-      break;
+  // Cartridge RPM
+  //   (pr tick per rotation if using tracking wheels)
+  ,600
 
-    default:
-      break;
-  }
-}
+  // External Gear Ratio 
+  //    (or gear ratio of tracking wheel)
+  ,1.66666666667
 
-// Global for updating SD
-void update_auto_sd();
+  // Uncomment if using tracking wheels
+  /*
+  // Left Tracking Wheel Ports (negative port will reverse it!)
+  ,{1, 2}
+  // Right Tracking Wheel Ports (negative port will reverse it!)
+  ,{3, 4}
+  */
 
-// Page up/down
-void
-page_up() {
-  if(current_page==num_of_pages-1)
-    current_page=0;
-  else
-    current_page++;
-  update_auto_sd();
-  auto_select(false);
-}
+  // Uncomment if tracking wheels are plugged into a 3 wire expander
+  // 3 Wire Port Expander Smart Port
+  // ,1
+);
 
-void
-page_down() {
-  if(current_page==0)
-    current_page=num_of_pages-1;
-  else
-    current_page--;
-  update_auto_sd();
-  auto_select(false);
-}
-
-
-/**
- * Store the last page to the micro SD card
- *
- * This allows you to select your autonomous mode while in the queue or in the pits.
- * If you powercycle the robot or turn off the code, the autonomous mode you selected
- * will still hold.
- */
-void
-update_auto_sd() {
-  // If no SD card, return
-  if (!IS_SD_CARD) return;
-
-  FILE* usd_file_write = fopen("/usd/auto.txt", "w");
-  std::string cp_str = std::to_string(current_page);
-  char const *cp_c = cp_str.c_str();
-  fputs(cp_c, usd_file_write);
-  fclose(usd_file_write);
-}
-
-void
-init_auto_sd() {
-  // If no SD card, return
-  if (!IS_SD_CARD)  return;
-
-  // Auton Selector
-  FILE* usd_file_read = fopen("/usd/auto.txt", "r");
-  char buf[5];
-  fread(buf, 1, 5, usd_file_read);
-  current_page = std::stoi(buf);
-  fclose(usd_file_read);
-
-  if(current_page>num_of_pages-1 || current_page<0)
-    current_page=0;
-  update_auto_sd();
-}
 
 
 /**
@@ -129,28 +49,45 @@ init_auto_sd() {
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void
-initialize() {
-  print_ez_template();
-  pros::delay(500);
+void initialize() {
+  // Print our branding over your terminal :D
+  ez::print_ez_template();
 
-  if (!IS_SD_CARD) printf("No SD Card Found!\n");
+  pros::delay(500); // Stop the user from doing anything while legacy ports configure.
 
-  disable_all_tasks();
+  // Check if SD card is pluged in
+  if (!ez::util::IS_SD_CARD) printf("No SD Card Found!\n");
 
-  init_auto_sd();
-  init_curve_sd();
+  // Configure your chassis controls
+  chassis.init_curve_sd(); // Initialize the input curves with what's on the SD card (this does nothing if there's no SD card)
+  chassis.toggle_modify_curve_with_controllerr(true); // Enables modifying the controller curve with buttons on the joysticks
+  chassis.set_active_brake(0.1); // Sets the active brake KP. 
 
+  // These are already defaulted to these buttons, but you can change the left/right curve buttons here!
+  // chassis.set_left_curve_buttons (pros::E_CONTROLLER_DIGITAL_LEFT, pros::E_CONTROLLER_DIGITAL_RIGHT);
+  // chassis.set_right_curve_buttons(pros::E_CONTROLLER_DIGITAL_Y,    pros::E_CONTROLLER_DIGITAL_A);
+
+  // Autonomous Selector using LLEMMU
+  ez::as::autoSelector.AddAutons({
+    Auton("Autonomous 1\nDoes Something", auto1),
+    Auton("Autonomous 2\nDoes Something Else", auto2),
+    Auton("Autonomous 3\nDoes Something More", auto3),
+  });
+
+  // Initialize auto selector and LLEMU
   pros::lcd::initialize();
-  auto_select(false);
-  pros::lcd::register_btn0_cb(page_down);
-  pros::lcd::register_btn2_cb(page_up);
-  if(!imu_calibrate()) {
-    pros::lcd::set_text(7, "IMU failed to calibrate!");
-  }
+  ez::as::init_auton_selector();
 
-  chassis_motor_init();
+  // Callbacks for auto selector
+  ez::as::autoSelector.PrintSelectedAuto();
+  pros::lcd::register_btn0_cb(ez::as::page_down);
+  pros::lcd::register_btn2_cb(ez::as::page_up);
+
+  // Calibrate IMU
+  if (!chassis.imu_calibrate())
+    pros::lcd::set_text(7, "IMU failed to calibrate!");
 }
+
 
 
 /**
@@ -158,10 +95,10 @@ initialize() {
  * the VEX Competition Switch, following either autonomous or opcontrol. When
  * the robot is enabled, this task will exit.
  */
-void
-disabled() {
-  disable_all_tasks();
+void disabled() {
+  // . . .
 }
+
 
 
 /**
@@ -173,10 +110,10 @@ disabled() {
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-void
-competition_initialize() {
-  disable_all_tasks();
+void competition_initialize() {
+  // . . .
 }
+
 
 
 /**
@@ -190,15 +127,14 @@ competition_initialize() {
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void
-autonomous() {
-  tare_gyro();
-  reset_drive_sensor();
-  set_drive_brake(MOTOR_BRAKE_HOLD);
-  drive_pid.resume();
+void autonomous() {
+  chassis.reset_gyro(); // Reset gyro position to 0
+  chassis.reset_drive_sensor(); // Reset drive sensors to 0
+  chassis.set_drive_brake(MOTOR_BRAKE_HOLD); // Set motors to hold.  This helps autonomous consistency. 
 
-  auto_select(true);
+  ez::as::autoSelector.CallSelectedAuto(); // Calls selected auton from autonomous selector. 
 }
+
 
 
 /**
@@ -214,15 +150,20 @@ autonomous() {
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-void
-opcontrol() {
-  drive_pid.suspend();
-  reset_drive_sensor();
-  set_drive_brake(MOTOR_BRAKE_COAST); // This is preference to what you like to drive on
+void opcontrol() {
+  // This is preference to what you like to drive on.
+  chassis.set_drive_brake(MOTOR_BRAKE_COAST);
 
   while (true) {
-    chassis_joystick_control();
 
-    pros::delay(DELAY_TIME);
-  }
-}
+    chassis.tank(); // Tank control
+    // chassis.arcade_standard(ez::SPLIT); // Standard split arcade
+    // chassis.arcade_standard(ez::SINGLE); // Standard single arcade
+    // chassis.arcade_flipped(ez::SPLIT); // Flipped split arcade
+    // chassis.arcade_flipped(ez::SINGLE); // Flipped single arcade
+
+    // . . .
+    // Put more user control code here!
+    // . . .
+
+    pros::delay(ez::util::DELAY_TIME); // This is used for timer calculations!  Keep this ez::util::DELAY_TIME
