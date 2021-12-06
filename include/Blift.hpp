d326d62b5ac6dacf5a3fcea9454026c0a963e21d
Mactar_Gueye <Mactarg1233@gmail.com>
2021-12-05 18:08:49 -0800

    Blift.hpp made

---
+++ include/Blift.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+void set_Blift(int input);
+void zero_Blift();
+int get_Blift();
+int get_Blift_vel();
+
+void set_Blift_position(int target);
+
+void Blift_control();
--- src/Subsystems/Blift.cpp
+++ src/Subsystems/Blift.cpp
@@ -15,14 +15,14 @@ pros::Motor blift(6, MOTOR_GEARSET_36, false, MOTOR_ENCODER_DEGREES);
 pros::ADIDigitalIn Tare_Blift_Limit_Switch ('C')
 
 
-void set_lift(int input)  { lift = input; }
+void set_Blift(int input)  { lift = input; }
 
-void zero_lift()    { lift.tare_position(); }
-int  get_lift()     { return lift.get_position(); }
-int  get_lift_vel() { return lift.get_actual_velocity(); }
+void zero_Blift()    { lift.tare_position(); }
+int  get_Blift()     { return lift.get_position(); }
+int  get_Blift_vel() { return lift.get_actual_velocity(); }
 
 void
-set_lift_position(int target, int speed) {
+set_Blift_position(int target, int speed) {
   lift.move_absolute(target, speed);
 }
 
